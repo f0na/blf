ccd43f20b7cf7a46adb88dc2489500b36eb367f5
nna <nna@fufu.moe>
2026-09-16 01:21:24 +0800

    fix: 关于页卡片布局调整并移除调试输出

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@ import Empty from "@/components/Empty";
 import Footer from "@/components/Footer";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { buttonVariants } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { SiteUptime } from "@/components/Uptime";
 import { get_about_data } from "@/lib/fetch";
 import { SocialType } from "@/lib/site";
@@ -15,16 +15,18 @@ export default async function AboutPage() {
             <main className="flex w-[61.8%] flex-col items-center py-2">
                 {about_data.site.name ? (
                     <Card className="flex w-full min-h-[50vh]">
-                        <CardHeader className="flex flex-row items-center gap-2">
-                            <Avatar size="lg">
-                                <AvatarImage src={about_data.site.icon} />
-                                <AvatarFallback>fufu</AvatarFallback>
-                            </Avatar>
-                            <span>{about_data.site.name}</span>
+                        <CardHeader className="flex flex-col">
+                            <CardTitle className="flex flex-row items-center gap-2">
+                                <Avatar size="lg">
+                                    <AvatarImage src={about_data.site.icon} />
+                                    <AvatarFallback>fufu</AvatarFallback>
+                                </Avatar>
+                                <span>{about_data.site.name}</span>
+                            </CardTitle>
+                            <CardDescription className="px-1">
+                                {about_data.site.about}
+                            </CardDescription>
                         </CardHeader>
-                        <CardDescription className="px-4">
-                            {about_data.site.about}
-                        </CardDescription>
                         <CardContent className="flex flex-1 justify-start items-start">
                             {about_data.social.map((item) => (
                                 <div className="flex flex-row items-center" key={item.value}>
@@ -64,9 +66,6 @@ export default async function AboutPage() {
                 ) : (
                     <Empty></Empty>
                 )}
-                <div>now: {Date.now()}<br/></div>
-                <div>create: {about_data.site.create_at}<br/></div>
-                <div>timestamp: {new Date(about_data.site.create_at!).getTime()}<br/></div>
             </main>
             <footer className="flex">
                 <Footer site={about_data.site}></Footer>
